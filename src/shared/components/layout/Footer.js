f542451732b44fe4bb7588d099510c518bbfd292
dkwilson <general@ytechnology.net>
2020-06-07 23:27:18 -0400

    first commit; project complete

---
+++ src/shared/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <footer>&copy;Practice Inc.{new Date().getFullYear()}</footer>
+  </div>
+  
+);
+
+export default Footer;
